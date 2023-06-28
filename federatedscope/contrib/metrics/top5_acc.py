b00882c3d32237dada15e6d86757d7046a582df0
AA_Ashley <2216660310@qq.com>
2023-06-28 10:06:14 +0800

    cifar10 10 clients 跑完

--- federatedscope/contrib/metrics/top5_acc.py
+++ federatedscope/contrib/metrics/top5_acc.py
@@ -1,10 +1,22 @@
 import torch
+import  numpy as np
 
 from federatedscope.register import register_metric
 
 METRIC_NAME = 'top5'
 
-# def Top5(ctx, y_true, y_pred, **kwargs ):
-#     _,max5 = torch.topk(y_pred, 5, dim=-1)
-#     y_true = y_true.view(-1,1)
-#     top5 += (y_true == max5).sum().item()
+def Top5(ctx, **kwargs ):
+    max5 = np.argsort(ctx.ys_prob,axis=-1)[:, -5:]
+    y_true = ctx.ys_true[:, np.newaxis]
+    top5 = np.sum(y_true == max5)
+    top5acc = round(100 * top5 / len(y_true), 2)
+    return top5acc
+
+def call_my_metric(types):
+    if METRIC_NAME in types:
+        the_larger_the_better = True
+        metric_builder = Top5
+        return METRIC_NAME, metric_builder, the_larger_the_better
+
+
+register_metric(METRIC_NAME, call_my_metric)
