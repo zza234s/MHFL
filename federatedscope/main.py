7f162de058f6b4d3fedb7146bcdf4e8cac2f83f0
zza234s <2214792801@qq.com>
2023-07-12 09:54:55 +0800

    update main.py

--- federatedscope/main.py
+++ federatedscope/main.py
@@ -66,5 +66,5 @@ if __name__ == '__main__':
     client_summarized_test_acc = _['client_summarized_avg']['test_acc']
     # client_summarized_weighted_avg = _['client_summarized_weighted_avg']['test_acc']
     print(f'client_summarized_test_acc:{client_summarized_test_acc}')
-    result_to_csv(_,init_cfg)
+    result_to_csv(_,init_cfg,best_round)
     # print(f'client_summarized_weighted_avg:{client_summarized_weighted_avg}')
